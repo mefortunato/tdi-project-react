1d12f1daed78ef9c68be1708b4023df580d27665
Michael E. Fortunato <mef231@gmail.com>
2018-05-27 14:35:21 -0400

    testing fetch - cors issue

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ticker: '',
+      reasultData: null,
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({ticker: event.target.value});
+  }
+  
+  handleSubmit(event) {
+    fetch('https://www.quandl.com/api/v3/datatables/WIKI/PRICES?ticker='+this.state.ticker+'&api_key=PyewNKgseooV1caGxUtG')
+      .then(
+        (res) => {
+          console.log(res);
+        },
+        (error) => {
+          console.log(error);
+        })
+    event.preventDefault();
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.ticker} onChange={this.handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
+        <div> {this.state.resultData} </div>
       </div>
     );
   }
