67d57ffe77a66b27bc8445f513a138f4abdc4ce1
Michael E. Fortunato <mef231@gmail.com>
2018-05-28 01:39:11 -0400

    switched to bootstrap-react fields

--- src/App.js
+++ src/App.js
@@ -1,68 +1,117 @@
 import React, { Component } from 'react';
 import Plot from 'react-plotly.js';
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import { DateRangePicker } from 'react-dates';
-import moment from 'moment';
 import './App.css';
+
+import { Form, FormGroup, ControlLabel, FormControl, Button  } from 'react-bootstrap';
+var DatePicker = require("react-16-bootstrap-date-picker");
 
 class App extends Component {
   constructor(props) {
     super(props);
-    var today = new moment();
+    var defaultEnd = new Date();
+    var defaultStart = new Date(defaultEnd.getFullYear(), defaultEnd.getMonth()-1, defaultEnd.getDate())
     this.state = {
       ticker: '',
       data: [],
-      startDate: new moment().subtract(1, 'months'),
-      endDate: today,
-      focusedInput: null,
+      maxDate: defaultEnd.toISOString().substring(0, 10),
+      minDate: defaultStart.toISOString().substring(0, 10),
+      startDate: defaultStart.toISOString().substring(0, 10),
+      endDate: defaultEnd.toISOString().substring(0, 10),
       yMin: 0,
       yMax: 100,
+      symbols: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleDateChangeStart = this.handleDateChangeStart.bind(this);
+    this.handleDateChangeEnd = this.handleDateChangeEnd.bind(this);
+  }
+  
+  componentDidMount() {
+    fetch('https://api.iextrading.com/1.0/ref-data/symbols')
+      .then(resp => resp.json())
+      .then(json => {
+        console.log(json.map(obj => obj.symbol))
+      })
   }
   
   handleChange(event) {
     this.setState({ticker: event.target.value});
   }
   
-  handleDateChange({ startDate, endDate }) {
-    var yMin = this.state.yMin;
-    var yMax = this.state.yMax;
+  handleDateChangeStart(value, formattedValue) {
+    var date = new Date(formattedValue);
     if (this.state.data.length > 0) {
-      var startIndex = this.state.data[0]['x'].indexOf(startDate.format('YYYY-MM-DD'));
-      var stopIndex = this.state.data[0]['x'].indexOf(endDate.format('YYYY-MM-DD'));
-      var dataRange = this.state.data[0]['y'].slice(startIndex, stopIndex);
-      yMin = Math.min(...dataRange)-10;
-      yMax = Math.max(...dataRange)+10;
+      var dates = this.state.data[0]['x'];
+      var closestDate = dates.reduce(function(prev, curr) {
+        return (Math.abs(new Date(curr) - date) < Math.abs(new Date(prev) - date) ? curr : prev);
+      });
+      var startIndex = dates.indexOf(closestDate);
+      var endIndex = dates.indexOf(this.state.endDate);
+      var data = this.state.data[0]['y'].slice(startIndex, endIndex);
+      this.setState({
+        startDate: closestDate,
+        yMin: Math.min(...data)-10,
+        yMax: Math.max(...data)+10,
+      });
     }
-    this.setState({
-      startDate: startDate,
-      endDate: endDate,
-      yMin: yMin,
-      yMax: yMax,
-    })
+    else {
+      this.setState({startDate: formattedValue});
+    }
+  }
+  
+  handleDateChangeEnd(value, formattedValue) {
+    var date = new Date(formattedValue);
+    if (this.state.data.length > 0) {
+      var dates = this.state.data[0]['x'];
+      var closestDate = dates.reduce(function(prev, curr) {
+        return (Math.abs(new Date(curr) - date) < Math.abs(new Date(prev) - date) ? curr : prev);
+      });
+      var startIndex = dates.indexOf(this.state.startDate);
+      var endIndex = dates.indexOf(closestDate);
+      var data = this.state.data[0]['y'].slice(startIndex, endIndex);
+      this.setState({
+        endDate: closestDate,
+        yMin: Math.min(...data)-10,
+        yMax: Math.max(...data)+10,
+      });
+    }
+    else {
+      this.setState({endDate: formattedValue});
+    }
   }
   
   handleSubmit(event) {
     fetch('https://api.iextrading.com/1.0/stock/'+this.state.ticker+'/chart/5y')
-      .then(resp => resp.json())
+      .then(function(resp) {
+        if (!resp.ok) {
+            throw Error(resp.statusText);
+        }
+        return resp.json();
+      })
       .then(json => {
         if (json.length > 0) {
-          console.log(json)
-          this.setState({data: [
-            {
-              x: json.map(obj => obj.date),
-              y: json.map(obj => Number(obj.close)),
-            }
-          ]})
+          var dates = json.map(obj => obj.date);
+          var prices = json.map(obj => Number(obj.close))
+          this.setState({
+            data: [{
+                x: dates,
+                y: prices,
+            }],
+            startDate: dates[0],
+            endDate: dates[dates.length-1],
+            minDate: dates[0],
+            maxDate: dates[dates.length-1]
+          })
         }
         else {
           alert('Cannot query '+this.state.ticker)
-          this.setState({data: []})
+          
         }
+      })
+      .catch(error => {
+        this.setState({data: []})
+        alert('Cannot query ticker: '+this.state.ticker);
       })
     event.preventDefault();
   }
@@ -70,38 +119,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.ticker} onChange={this.handleChange} />
-          <input type="submit" value="Submit" />
-          <div>
-            Pick Date Range:
-            <DateRangePicker 
-              small
-              noBorder
-              showDefaultInputIcon
-              startDateId="startDate"
-              endDateId="endDate"
-              startDate={this.state.startDate}
-              endDate={this.state.endDate}
-              onDatesChange={this.handleDateChange}
-              focusedInput={this.state.focusedInput}
-              onFocusChange={(focusedInput) => { this.setState({ focusedInput })}}
-              isOutsideRange={(date) => {
-                if (this.state.data.length > 0) {
-                  if (this.state.data[0]['x'].includes(date.format('YYYY-MM-DD'))) {
-                    return false
-                  }
-                  else {
-                    return true
-                  }
-                }
-                else {
-                  return false
-                }
-              }}
+        <Form inline onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <ControlLabel>Ticker</ControlLabel>{' '}
+            <FormControl type="text" placeholder="GOOG" value={this.state.ticker} onChange={this.handleChange}/>
+          </FormGroup>{' '}
+          <Button type="submit">Submit</Button>
+        </Form>
+        <Form inline>
+          <FormGroup>
+            <ControlLabel>Date Range</ControlLabel>{' '}
+            <DatePicker 
+              id="start-datepicker"
+              style={{width: "150px"}}
+              dateFormat="YYY-MM-DD"
+              maxDate={this.state.maxDate}
+              minDate={this.state.minDate}
+              value={this.state.startDate}
+              onChange={this.handleDateChangeStart}
             />
-          </div>
-        </form>
+            <DatePicker
+              id="end-datepicker"
+              style={{width: "150px"}}
+              dateFormat="YYY-MM-DD"
+              maxDate={this.state.maxDate}
+              minDate={this.state.minDate}
+              value={this.state.endDate}
+              onChange={this.handleDateChangeEnd}
+            />
+          </FormGroup>{' '}
+        </Form>
         <Plot
           data={this.state.data}
           layout={
@@ -112,7 +159,7 @@ class App extends Component {
                 showgrid: false,
                 zeroline: false,
                 showline: true,
-                range: [this.state.startDate.toDate(), this.state.endDate.toDate()],
+                range: [this.state.startDate, this.state.endDate],
                 linecolor: 'black',
                 linewidth: 2,
                 mirror: true
